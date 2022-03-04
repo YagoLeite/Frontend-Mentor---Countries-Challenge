83d5475850b60fcc7daff952200cc38268a015b9
Yago Batista <yagobatistafis@yahoo.com.br>
2022-03-04 14:38:55 -0300

    next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains:
-      "https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_the_Taliban.svg",
-  },
 };
 
 module.exports = nextConfig;
